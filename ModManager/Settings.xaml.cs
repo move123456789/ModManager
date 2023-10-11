e727ce69b313c9af8e2c446eca53ea6c9785db35
SmokyAce <julian.hansen2002@gmail.com>
2023-10-11 11:31:10 +0200

    Settings is now saved

--- ModManager/Settings.xaml.cs
+++ ModManager/Settings.xaml.cs
@@ -1,37 +1,75 @@
 using System.Diagnostics;
-using Windows.Gaming.Input;
-using Windows.Gaming.UI;
+
 
 namespace ModManager;
 
 public partial class Settings : ContentPage
 {
+
 	public Settings()
 	{
 		InitializeComponent();
+
         GameDirSaveButton.Clicked += OnGameDirSaveButtonButtonClicked;
         ModDirSaveButton.Clicked += OnModDirSaveButtonButtonClicked;
+
+
         CheckAndSetGamePath();
         CheckAndSetModPath();
     }
 
+    public string savedGameDir
+    {
+        get { return Preferences.Get(nameof(savedGameDir), null); }
+        set { Preferences.Set(nameof(savedGameDir), value); }
+    }
+
+    public string savedModDir
+    {
+        get { return Preferences.Get(nameof(savedModDir), null); }
+        set { Preferences.Set(nameof(savedModDir), value); }
+    }
+
     private void CheckAndSetGamePath()
     {
-        if (GenericFunctions.GetGameDir() == null)
+        //if (GenericFunctions.GetGameDir() == null)
+        //{
+        //    GameDirectoryEntry.Text = "Game Directory Not Found, Please Enter";
+        //    DisplayAlert("Alert", "Game Directory Not Found, Please Enter", "OK");
+        //    Debug.WriteLine("Game Directory Not Found, Please Enter");
+        //} else
+        //{
+        //    GameDirectoryEntry.Text = GenericFunctions.GetGameDir();
+        //    Debug.WriteLine($"Game Directory Found: {GenericFunctions.GetGameDir()}");
+        //}
+
+        if (string.IsNullOrEmpty(savedGameDir))
         {
             GameDirectoryEntry.Text = "Game Directory Not Found, Please Enter";
             DisplayAlert("Alert", "Game Directory Not Found, Please Enter", "OK");
             Debug.WriteLine("Game Directory Not Found, Please Enter");
-        } else
+        }
+        else
         {
-            GameDirectoryEntry.Text = GenericFunctions.GetGameDir();
-            Debug.WriteLine($"Game Directory Found: {GenericFunctions.GetGameDir()}");
+            GameDirectoryEntry.Text = savedGameDir;
+            Debug.WriteLine($"Game Directory Found: {savedGameDir}");
         }
     }
 
     private void CheckAndSetModPath()
     {
-        if (GenericFunctions.GetModDir() == null)
+        //if (GenericFunctions.GetModDir() == null)
+        //{
+        //    ModDirectoryEntry.Text = "Mod Directory Not Found, Are You Sure RedLoader Is Installed?";
+        //    DisplayAlert("Alert", "Mod Directory Not Found, Are You Sure RedLoader Is Installed?", "OK");
+        //    Debug.WriteLine("Mod Directory Not Found, Are You Sure RedLoader Is Installed?");
+        //}
+        //else
+        //{
+        //    ModDirectoryEntry.Text = GenericFunctions.GetModDir();
+        //    Debug.WriteLine($"Game Directory Found: {GenericFunctions.GetModDir()}");
+        //}
+        if (string.IsNullOrEmpty(savedModDir))
         {
             ModDirectoryEntry.Text = "Mod Directory Not Found, Are You Sure RedLoader Is Installed?";
             DisplayAlert("Alert", "Mod Directory Not Found, Are You Sure RedLoader Is Installed?", "OK");
@@ -39,8 +77,8 @@ public partial class Settings : ContentPage
         }
         else
         {
-            ModDirectoryEntry.Text = GenericFunctions.GetModDir();
-            Debug.WriteLine($"Game Directory Found: {GenericFunctions.GetModDir()}");
+            ModDirectoryEntry.Text = savedModDir;
+            Debug.WriteLine($"Mod Directory Found: {savedModDir}");
         }
     }
 
@@ -49,6 +87,7 @@ public partial class Settings : ContentPage
         // Your code to execute when the button is clicked
         Debug.WriteLine("Save button clicked!");
         GenericFunctions.gameDir = GameDirectoryEntry.Text;
+        savedGameDir = GameDirectoryEntry.Text;
         DisplayAlert("Note", "Game Directory Saved", "OK");
 
     }
@@ -61,4 +100,26 @@ public partial class Settings : ContentPage
         DisplayAlert("Note", "Mod Directory Saved", "OK");
 
     }
+
+    //public async Task LoadSaveSettings()
+    //{
+    //    if (settingsService == null)
+    //    {
+    //        Debug.WriteLine("settingsService is null!");
+    //        return;
+    //    }
+
+    //    savedGameDir = await settingsService.Get<string>(nameof(savedGameDir), null);
+    //    savedModDir = await settingsService.Get<string>(nameof(savedModDir), null);
+
+    //    Debug.WriteLine($"LoadSaveSettings SaveGameDir: {savedGameDir}");
+    //    Debug.WriteLine($"LoadSaveSettings SaveModDir: {savedModDir}");
+    //}
+
+    //public async Task SaveSettings()
+    //{
+    //    await settingsService.Save(nameof(savedGameDir), savedGameDir);
+    //    await settingsService.Save(nameof(savedModDir), savedModDir);
+    //    await DisplayAlert("Saved!", "Settings has been saved!", "OK");
+    //}
 }
